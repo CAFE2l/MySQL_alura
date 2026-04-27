26cc0a2fd7fbc620a22cabe843cfad08785b3201
Your Name <you@example.com>
2026-04-27 14:39:43 -0300

    class 03 finished

--- Aulas/Aula01/index.sql
+++ Aulas/Aula01/index.sql
@@ -2,6 +2,20 @@ CREATE DATABASE sucos;
 
 
 USE sucos;
+CREATE TABLE tbclientes (
+    cpf INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE tbclientes ADD PRIMARY KEY (cpf);
+
+ALTER TABLE tbclientes ADD COLUMN DATA_nascimento DATE;
+
+INSERT INTO tbclientes (cpf, nome, email, DATA_nascimento) VALUES (13390223975, 'João Silva', 'gutiajs@gmail.com', '1990-05-15');
+
+
+
 CREATE TABLE tbprodutos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -13,3 +27,9 @@ SELECT * FROM tbprodutos;
 
 INSERT INTO tbprodutos (nome, preco) VALUES ('Suco de Laranja', 5.99);
 INSERT INTO tbprodutos (nome, preco) VALUES ('Suco de Abacaxi', 6,99);
+
+
+
+select * from tbclientes;
+
+SELECT CPF, nome, endereo FROM tbclientes;
